39557137fdeb78dc0d1d055bfdd81cf354603e65
Sebastian Lancheros <sebastian.lancheros@proper.ai>
2024-10-28 12:07:51 -0500

    add webpack config for prod in order to deploy the site

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,9 +5,7 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
-    },
+    static: "./dist",
     port: 3000,
     open: true,
     hot: true,
